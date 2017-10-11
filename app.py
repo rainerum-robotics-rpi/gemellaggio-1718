2c9398788f3f74bad419c3dc9eee9ceefe27bac5
Julian Sanin <j54n1n+noreply@gmail.com>
2017-10-11 08:42:24 +0000

    Prepare for release

--- app.py
+++ app.py
@@ -2,17 +2,18 @@
 
 import rgem_gmail
 import rgem_cam
+from gpiozero import LED
+from time import sleep
 
 def main():
   to = "j54n1n+noreply@gmail.com"
   sender = "rgem1718+1@gmail.com"
-  subject = "Test"
-  msgHtml = "Hi<br/>Html Email"
-  msgPlain = "Hi\nPlain Email"
-  #rgem_gmail.SendMessage(sender, to, subject, msgHtml, msgPlain)
-  #rgem_gmail.SendMessage(sender, to, subject, msgHtml, msgPlain, ["chip.png"])
-  rgem_cam.TakePicture()
-  #rgem_gmail.NewMessages(sender)
+  subject = "Rainerum Gemellaggio 2017/2018"
+  msgHtml = "Hi<br/>Here is your picture !-)"
+  msgPlain = "Hi\nHere is your picture !-)"
+  # This is a comment.
+  # Add your code below this comment and before if __name__ == '__main__':
+  
 
 if __name__ == '__main__':
   main()
